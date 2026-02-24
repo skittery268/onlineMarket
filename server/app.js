fc83ab26e0cd37f933a8f522a6929939883733a2
skittery268 <sabadzidzikashvili6@gmail.com>
2026-02-24 18:06:51 +0400

    Online Market

--- server/app.js
+++ server/app.js
@@ -30,11 +30,13 @@ app.use("/api/whishlist", whishListRouter);
 
 app.use("/api/admin", adminRouter);
 
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("connection to the database was successful");
 
-        app.listen(3000, () => {
+        app.listen(PORT, () => {
             console.log("Server Started!");
         })
     })
