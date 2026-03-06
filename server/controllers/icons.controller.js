408df081c8001c7489acfe4207da1aef72165116
skittery268 <sabadzidzikashvili6@gmail.com>
2026-03-07 01:35:10 +0400

    Online Market

--- server/controllers/icons.controller.js
+++ server/controllers/icons.controller.js
@@ -4,8 +4,6 @@ const getAllIcons = async (req, res) => {
     try {
         const icons = await Icon.find();
 
-        console.log(icons);
-
         res.status(200).json(icons);
     } catch (err) {
         res.status(500).json(err.message);
